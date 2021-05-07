ee29e5be9ab89f3d39251e99fdb969df9c749251
Carl-Henric <carlhenric.grip@gmail.com>
2021-05-07 21:08:33 +0000

    Added styling to header

--- oldandgold/settings.py
+++ oldandgold/settings.py
@@ -1,4 +1,3 @@
-import os
 """
 Django settings for oldandgold project.
 
@@ -10,6 +9,8 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+
+import os
 
 from pathlib import Path
 
@@ -147,6 +148,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
