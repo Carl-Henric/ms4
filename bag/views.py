33f52ad6043262c6e38d486af328bf673044b523
Carl-Henric <carlhenric.grip@gmail.com>
2021-05-11 10:51:18 +0000

    Added functionality to add to bag button

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -7,3 +7,16 @@ def view_bag(request):
     """ The view for rendering shopping bag """
     
     return render(request, 'bag/bag.html')
+
+
+def add_to_bag(request, item_id):
+    """ Adding 1 of each product to shopping bag """
+
+    redirect_url = request.POST.get('redirect_url')
+    bag = request.session.get('bag', {})
+
+    bag[item_id] = 1
+
+    request.session['bag'] = bag
+    print(request.session['bag'])
+    return redirect(redirect_url)
