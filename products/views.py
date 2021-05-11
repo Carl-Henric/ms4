47fc9bc6cd0ec036c9ce8d5e90bc253a39071626
Carl-Henric <carlhenric.grip@gmail.com>
2021-05-11 07:55:51 +0000

    Added categories and functionality to navigation

--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Product
+from .models import Product, Category
 
 # Create your views here.
 
@@ -8,6 +8,13 @@ def all_products(request):
     """ Displaying all products """
 
     products = Product.objects.all()
+    categories = None
+
+    if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
 
     context = {
         'products': products, 
@@ -23,6 +30,7 @@ def product_detail(request, product_id):
 
     context = {
         'product': product, 
+        'current_categories': categories,
     }
     
     return render(request, 'products/product_detail.html', context)
