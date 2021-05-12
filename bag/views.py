e8773f520e89fe382dde1b61ab2d1dc327b22bc8
Carl-Henric <carlhenric.grip@gmail.com>
2021-05-12 13:06:49 +0000

    Added toast messages and styling to toast success

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, HttpResponse
+from django.shortcuts import render, redirect, HttpResponse, get_object_or_404
 from django.contrib import messages
 
 from products.models import Product
@@ -15,11 +15,11 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ Adding 1 of each product to shopping bag """
 
-    product = Product.objects.get(pk=item_id)
+    product = get_object_or_404(Product, pk=item_id)
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
 
-    messages.success(request, f'{product.brand} {product.name} added to your bag')
+    messages.success(request, f'{product.year}´s {product.brand} {product.name} added to your bag')
 
     bag[item_id] = 1
 
@@ -29,12 +29,16 @@ def add_to_bag(request, item_id):
 
 def remove_from_bag(request, item_id):
     """ Removing item from shopping bag  """
+
     try:
+        product = get_object_or_404(Product, pk=item_id)
         bag = request.session.get('bag', {})
 
         bag.pop(item_id)
+        messages.success(request, f'{product.year}´s {product.brand} {product.name} Removed from your bag')
 
         request.session['bag'] = bag
         return HttpResponse(status=200)
     except Exception as e:
+        messages.error(request, f'Error removing watch: {e}')
         return HttpResponse(status=500)
