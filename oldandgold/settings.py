730173f61ac4c63c6002aa5aa742311da508d14d
Carl-Henric <carlhenric.grip@gmail.com>
2021-05-26 11:34:22 +0000

    Started deployment to Heroku

--- oldandgold/settings.py
+++ oldandgold/settings.py
@@ -14,6 +14,8 @@ import os
 if os.path.exists("env.py"):
   import env
 
+import dj_database_url
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -124,9 +126,10 @@ WSGI_APPLICATION = 'oldandgold.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+ }
+
 
 
 # Password validation
