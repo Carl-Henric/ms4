b76e4e999ed3400c253014028ac9724ce2b562b4
Carl-Henric <carlhenric.grip@gmail.com>
2021-05-26 14:23:54 +0000

    Added custom storages and AWS

--- oldandgold/settings.py
+++ oldandgold/settings.py
@@ -55,6 +55,8 @@ INSTALLED_APPS = [
 
     # Other apps
     'crispy_forms',
+    'storages',
+
 ]
 
 MIDDLEWARE = [
@@ -178,6 +180,24 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+if 'USE_AWS' in os.environ:
+    # AWS BUCKET CONFIG
+    AWS_STORAGE_BUCKET_NAME = 'oldandgold'
+    AWS_S3_REGION_NAME = 'eu-north-1'
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+    # STATIC AND MEDIA FILES
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    STATICFILES_LOCATION = 'static'
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+    MEDIAFILES_LOCATION = 'media'
+
+    # OVERIDE STATIC AND MEDIA URL´s In PRODUCTION
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
 
 # Default primary key field type
